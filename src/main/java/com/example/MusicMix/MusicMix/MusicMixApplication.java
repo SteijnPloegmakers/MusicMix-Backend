4025127780bddfa8ce573fd2ba7cef3294e5d303
Steijn Ploegmakers <461991@student.fontys.nl>
2021-11-08 14:57:31 +0100

    Initial commit

---
+++ src/main/java/com/example/MusicMix/MusicMix/MusicMixApplication.java
@@ -0,0 +1,13 @@
+package com.example.MusicMix.MusicMix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicMixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicMixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MusicMix/MusicMix/MusicMixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MusicMix.MusicMix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicMixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
