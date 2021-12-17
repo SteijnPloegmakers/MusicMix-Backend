4bb6a9df9b30094d0d7e733fba673dd67e7d9974
Steijn Ploegmakers <461991@student.fontys.nl>
2021-12-17 14:18:21 +0100

    feature: load test made

---
+++ test.js
@@ -0,0 +1,28 @@
+import http from "k6/http";
+import {sleep, check} from "k6";
+
+export let options ={
+    insecureSkipTLSVerify : true,
+    noConnectionReuse : false,
+    stages:[
+        {duration : '5m', target : 150},
+        {duration : '10m', target : 150},
+        {duration: '5m', target: 0}
+    ],
+    tresholds:{
+        http_req_duration:['p(99)<200'],
+        http_req_failed:['rate<0.01']
+    }
+}
+
+const base_url = 'http://localhost:8080'
+export default () => {
+    const responses = http.batch([
+        ['GET', `${base_url}/api/songs`]
+        ['GET', `${base_url}/api`]
+    ])
+    check(responses[0], {
+        'status is 200': res => res.status === 200
+    })
+    sleep(1);
+}
