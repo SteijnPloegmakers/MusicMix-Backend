c1dbc841ccff9183e430dec49a7e543d791d5cff
Steijn Ploegmakers <461991@student.fontys.nl>
2021-12-21 10:35:20 +0100

    feature: spike- and stresstest made

--- loadtest.js
+++ loadtest.js
@@ -1,6 +1,8 @@
 import http from "k6/http";
 import {sleep, check} from "k6";
 
+
+//Load Test
 export let options ={
     insecureSkipTLSVerify : true,
     noConnectionReuse : false,
@@ -9,7 +11,7 @@ export let options ={
         {duration : '1m', target : 150},
         {duration: '30s', target: 0}
     ],
-    tresholds:{
+    thresholds:{
         http_req_duration:['p(99)<200'],
         http_req_failed:['rate<0.01']
     }
@@ -19,10 +21,13 @@ const base_url = 'http://localhost:8080'
 export default () => {
     const responses = http.batch([
         ['GET', `${base_url}/api/songs`]
-        ['GET', `${base_url}/api`]
     ])
     check(responses[0], {
         'status is 200': res => res.status === 200
     })
     sleep(1);
 }
+
+//Spike Test
+
+//Stress Test
