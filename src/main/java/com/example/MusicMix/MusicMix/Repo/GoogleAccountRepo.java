ddfda207ba0b611c87427dfccb4fe8bbc8455996
Steijn Ploegmakers <461991@student.fontys.nl>
2022-01-07 16:02:42 +0100

    tweak: some code for pipeline

--- src/main/java/com/example/MusicMix/MusicMix/Repo/GoogleAccountRepo.java
+++ src/main/java/com/example/MusicMix/MusicMix/Repo/GoogleAccountRepo.java
@@ -2,9 +2,11 @@ package com.example.MusicMix.MusicMix.Repo;
 
 import com.example.MusicMix.MusicMix.Models.GoogleAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface GoogleAccountRepo extends JpaRepository<GoogleAccount, UUID> {
 
 }
--- src/main/java/com/example/MusicMix/MusicMix/Repo/SongRepo.java
+++ src/main/java/com/example/MusicMix/MusicMix/Repo/SongRepo.java
@@ -2,9 +2,11 @@ package com.example.MusicMix.MusicMix.Repo;
 
 import com.example.MusicMix.MusicMix.Models.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface SongRepo extends JpaRepository<Song, UUID> {
 
 }
