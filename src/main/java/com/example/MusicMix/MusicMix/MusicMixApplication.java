296797045b8686a3374870ffcdb7d3a868b96c85
Steijn Ploegmakers <461991@student.fontys.nl>
2022-01-07 16:00:26 +0100

    tweak: some code for pipeline

--- src/main/java/com/example/MusicMix/MusicMix/MusicMixApplication.java
+++ src/main/java/com/example/MusicMix/MusicMix/MusicMixApplication.java
@@ -2,6 +2,8 @@ package com.example.MusicMix.MusicMix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 
 @SpringBootApplication
@@ -9,6 +11,12 @@ public class MusicMixApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusicMixApplication.class, args);
+	}
+
+	@Bean(name="entityManagerFactory")
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		return sessionFactory;
 	}
 
 
